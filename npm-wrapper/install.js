d56b12c4be56333b2a03f24542b6597744fc306c
parjanyaacoder <padityashukla26@gmail.com>
2026-06-11 23:07:20 +0530

    fix: embed OAuth credentials during GoReleaser build for auth

--- npm-wrapper/install.js
+++ npm-wrapper/install.js
@@ -5,7 +5,7 @@ const https = require('https');
 const tar = require('tar');
 const unzipper = require('unzipper');
 
-const VERSION = '1.0.6';
+const VERSION = '1.0.7';
 const REPO = 'parjanyaacoder/another-meet';
 
 const platformMap = {
